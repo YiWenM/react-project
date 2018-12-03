b3b694d2a572af273ab8545c02d9a88814d523d1
Your Name <y18242016486@163.com>
2018-12-03 19:33:16 +0800

    22

---
+++ src/setupProxy.js
@@ -0,0 +1,18 @@
+const proxy = require('http-proxy-middleware');
+module.exports=function(app){
+	app.use(proxy('/pc',{
+		target:'https://huajuanmall.com',
+		host:'huajuanmall.com',
+		changeOrigin:true
+	}));
+	
+}
+//https://www.huajuanmall.com/pc/rec/single?cls_id=0&offset=0
+/*const proxy = require('http-proxy-middleware'); //模块 vue ,react 之所以支持反向代理
+        module.exports = function(app) {
+          app.use(proxy('/pc', {
+                  target: 'https://www.huajuanmall.com',
+                  host: 'huajuanmall.com',
+                  changeOrigin:true
+          }));
+        };*/
